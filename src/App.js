3f7bee25a1f9e3e1d55f7c2afe62d7500796cc1e
Henry Osorio Gómez <mo@Henrys-MacBook-Air.local>
2024-11-13 08:45:08 -0800

    Inser botom

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+function getTitle(title){
+  return title;
+}
+
+function App(){
+  return(
+    <div>
+      <h1>Hello {getTitle('world')}</h1>
+
+      <label htmlFor='search'>search:</label>
+      <input id='search' type='text'/>
     </div>
-  );
+  )
 }
 
 export default App;
